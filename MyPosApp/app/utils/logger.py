1f8ab8716b5339bd8f2d664d742b344881d94810
faia1109 <furandourusukaaretto@yahoo.co.jp>
2026-04-05 17:11:49 +0900

    ロガー設定機能の追加
    - ログファイルの作成と設定を行うsetup_logger関数を追加
    - エラーログのためのファイルハンドラを設定
    - 既存のハンドラがある場合は再利用

---
+++ MyPosApp/app/utils/logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from app.config import DATA_DIR
+def setup_logger():
+    os.makedirs(DATA_DIR, exist_ok=True)
+    log_file = os.path.join(DATA_DIR, 'app.log')
+    
+    logger = logging.getLogger('MyPosApp')
+    
+    # 既にハンドラが登録されていればそのまま返す
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.ERROR)
+    
+    # 単一ファイル・追記モード
+    fh = logging.FileHandler(log_file, mode='a', encoding='utf-8')
+    fh.setLevel(logging.ERROR)
+    
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    
+    logger.addHandler(fh)
+    return logger
